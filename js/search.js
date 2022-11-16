d5bcbc091b8897555d0fb6b2536e8bf36577fe80
Nizrum <62435919+Nizrum@users.noreply.github.com>
2022-11-16 17:53:36 +0400

    Поправил ссылку на страницу товаров
    Поправил ссылку на страницу товаров.

--- js/search.js
+++ js/search.js
@@ -37,7 +37,7 @@
                 localStorage.setItem("goods", JSON.stringify(array));
 
                 if (window.location.pathname !== "/goods.html") {
-                    window.location.href = "https://nizrum.github.io/wildberries/goods.html";
+                    window.location.pathname = "/goods.html";
                 } else {
                     renderGoods(array);
                 }
